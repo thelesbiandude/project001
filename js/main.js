68774bbcb64e3560087340707a4b642e86154f65
Devanshu Sehgal <devanshudukaans@gmail.com>
2018-05-20 16:10:33 +0530

    Imported JS files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// Jquery
 $(document).ready(() => {
     $('.sidenav').sidenav();
+});
+
+// Angular
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.mLists = [];
+    $scope.images = [];
+    
 });
